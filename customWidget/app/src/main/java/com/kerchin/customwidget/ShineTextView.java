91aecb7b9e9a4815abbe927397269f0b05750321
kerchin <kerchin>
2016-01-14 15:48:12 +0800

    16.1.14-1 customWidget自定义控件 ondraw尝试

---
+++ customWidget/app/src/main/java/com/kerchin/customwidget/ShineTextView.java
@@ -0,0 +1,58 @@
+package com.kerchin.customwidget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Kerchin on 2015/12/31 0031.
+ */
+public class ShineTextView extends TextView {
+    public ShineTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    int mViewWidth, mTranslate;
+    Paint mPaint;
+    LinearGradient mLinearGradient;
+    Matrix mGradientMatrix;
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mViewWidth == 0) {
+            mViewWidth = getMeasuredWidth();
+            if (mViewWidth > 0) {
+                mPaint = getPaint();
+                mLinearGradient = new LinearGradient(0, 0, mViewWidth, 0, new int[]{Color.BLUE, 0xffffffff, Color.BLUE}, null, Shader.TileMode.CLAMP);
+                mPaint.setShader(mLinearGradient);
+                mGradientMatrix = new Matrix();
+            }
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mGradientMatrix != null) {
+            mTranslate += mViewWidth / 5;
+            if (mTranslate > 2 * mViewWidth) {
+                mTranslate = -mViewWidth;
+            }
+            mGradientMatrix.setTranslate(mTranslate, 0);
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(100);
+        }
+    }
+}
