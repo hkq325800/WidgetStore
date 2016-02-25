2a5a9a3805e6ab659a6fe4c83530e63627174984
kerchin <kerchin>
2016-02-25 17:29:24 +0800

    16.2.25-1 SeeWeather add

---
+++ SeeWeather/app/src/main/java/com/xiecc/seeWeather/modules/domain/Setting.java
@@ -0,0 +1,74 @@
+package com.xiecc.seeWeather.modules.domain;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import com.xiecc.seeWeather.base.BaseApplication;
+
+/**
+ * 设置相关
+ * Created by hugo on 2016/2/19 0019.
+ */
+public class Setting {
+
+    public static final String CHANGE_ICONS = "change_icons";//切换图标
+    public static final String CLEAR_CACHE = "clear_cache";//清空缓存
+    public static final String AUTO_UPDATE = "change_update_time"; //自动更新时长
+    public static final String CITY_NAME = "城市";//选择城市
+    public static final String HOUR = "小时";//当前小时
+
+    public static final String API_TOKEN = "xxx";//fir.im api_token
+    public static final String KEY = "465726f235ca47aeb51346dc625a29ac";// 和风天气 key
+
+    public static int ONE_HOUR = 3600;
+
+    private static Setting sInstance;
+
+    private SharedPreferences mPrefs;
+
+
+    public static Setting getInstance() {
+        if (sInstance == null) {
+            sInstance = new Setting(BaseApplication.mAppContext);
+        }
+        return sInstance;
+    }
+
+
+    private Setting(Context context) {
+        mPrefs = context.getSharedPreferences("setting", Context.MODE_PRIVATE);
+        //mPrefs.edit().putInt(CHANGE_ICONS, 1).apply();
+    }
+
+
+    public Setting putBoolean(String key, boolean value) {
+        mPrefs.edit().putBoolean(key, value).apply();
+        return this;
+    }
+
+
+    public boolean getBoolean(String key, boolean def) {
+        return mPrefs.getBoolean(key, def);
+    }
+
+
+    public Setting putInt(String key, int value) {
+        mPrefs.edit().putInt(key, value).apply();
+        return this;
+    }
+
+
+    public int getInt(String key, int defValue) {
+        return mPrefs.getInt(key, defValue);
+    }
+
+
+    public Setting putString(String key, String value) {
+        mPrefs.edit().putString(key, value).apply();
+        return this;
+    }
+
+
+    public String getString(String key, String defValue) {
+        return mPrefs.getString(key, defValue);
+    }
+}
---
+++ SeeWeather/app/src/main/java/com/xiecc/seeWeather/modules/ui/setting/SettingFragment.java
@@ -0,0 +1,116 @@
+package com.xiecc.seeWeather.modules.ui.setting;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import com.xiecc.seeWeather.R;
+import com.xiecc.seeWeather.base.BaseApplication;
+import com.xiecc.seeWeather.common.ACache;
+import com.xiecc.seeWeather.common.FileSizeUtil;
+import com.xiecc.seeWeather.modules.domain.Setting;
+
+/**
+ * Created by hugo on 2016/2/19 0019.
+ */
+public class SettingFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener {
+    private static String TAG = SettingFragment.class.getSimpleName();
+    //private SettingActivity mActivity;
+    private Setting mSetting;
+    private Preference mChangeIcons;
+    private Preference mChangeUpdate;
+    private Preference mClearCache;
+
+    private ACache mACache;
+
+    //public SettingFragment() {}
+    //@Override public void onAttach(Context context) {
+    //    super.onAttach(context);
+    //    mActivity = (SettingActivity) context;
+    //}
+
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.setting);
+        mSetting = Setting.getInstance();
+        mACache = ACache.get(getActivity());
+
+        mChangeIcons = findPreference(Setting.CHANGE_ICONS);
+        mChangeUpdate = findPreference(Setting.AUTO_UPDATE);
+        mClearCache = findPreference(Setting.CLEAR_CACHE);
+
+        mChangeIcons.setSummary(getResources().getStringArray(R.array.icons)[mSetting.getInt(Setting.CHANGE_ICONS, 0)]);
+        mChangeUpdate.setSummary(
+                getResources().getStringArray(R.array.cache_time)[mSetting.getInt(Setting.AUTO_UPDATE, 0)]);
+        mClearCache.setSummary(FileSizeUtil.getAutoFileOrFilesSize(BaseApplication.cacheDir));
+
+        mChangeIcons.setOnPreferenceClickListener(this);
+        mChangeUpdate.setOnPreferenceClickListener(this);
+        mClearCache.setOnPreferenceClickListener(this);
+    }
+
+
+    @Override public boolean onPreferenceClick(Preference preference) {
+        if (mChangeIcons == preference) {
+            showIconDialog();
+        }
+        else if (mClearCache == preference) {
+            mACache.clear();
+            mClearCache.setSummary(FileSizeUtil.getAutoFileOrFilesSize(BaseApplication.cacheDir));
+            Snackbar.make(getView(), "缓存已清除", Snackbar.LENGTH_SHORT).show();
+        }
+        else if (mChangeUpdate == preference) {
+            showUpdateDialog();
+        }
+
+        return false;
+    }
+
+
+    private void showIconDialog() {
+        new AlertDialog.Builder(getActivity()).setTitle("更换图标")
+                                              .setSingleChoiceItems(getResources().getStringArray(R.array.icons),
+                                                      mSetting.getInt(Setting.CHANGE_ICONS, 0),
+                                                      new DialogInterface.OnClickListener() {
+                                                          @Override
+                                                          public void onClick(DialogInterface dialog, int which) {
+                                                              if (which != mSetting.getInt(Setting.CHANGE_ICONS, 0)) {
+                                                                  mSetting.putInt(Setting.CHANGE_ICONS, which);
+                                                              }
+                                                              dialog.dismiss();
+                                                              mChangeIcons.setSummary(getResources().getStringArray(
+                                                                      R.array.icons)[mSetting.getInt(
+                                                                      Setting.CHANGE_ICONS, 0)]);
+                                                              Snackbar.make(getView(), "切换成功,重启应用生效",
+                                                                      Snackbar.LENGTH_SHORT).show();
+                                                          }
+                                                      })
+                                              .show();
+    }
+
+
+    private void showUpdateDialog() {
+        new AlertDialog.Builder(getActivity()).setTitle("更换频率")
+                                              .setSingleChoiceItems(getResources().getStringArray(R.array.cache_time),
+                                                      mSetting.getInt(Setting.AUTO_UPDATE, 0),
+                                                      new DialogInterface.OnClickListener() {
+                                                          @Override
+                                                          public void onClick(DialogInterface dialog, int which) {
+                                                              if (which != mSetting.getInt(Setting.AUTO_UPDATE, 0)) {
+                                                                  mSetting.putInt(Setting.AUTO_UPDATE, which);
+                                                              }
+                                                              dialog.dismiss();
+                                                              mChangeUpdate.setSummary(getResources().getStringArray(
+                                                                      R.array.cache_time)[mSetting.getInt(
+                                                                      Setting.AUTO_UPDATE, 0)]);
+
+                                                              Snackbar.make(getView(), "设置成功", Snackbar.LENGTH_SHORT)
+                                                                      .show();
+                                                          }
+                                                      })
+                                              .show();
+    }
+}
